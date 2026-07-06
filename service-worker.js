831b848373465d72fd66024fc5e059cfb4edfb73
nvthahv03-math <nvthahv03@gmail.com>
2026-07-06 21:51:48 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,16 +2,40 @@ const CACHE_NAME = "mathsphere-v1";
 
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./maskable-512.png"
 ];
 
+// Cài đặt
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// Kích hoạt
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Đọc cache
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
